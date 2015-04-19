a8adfd04052798db762a750cca439039a75b09c0
Terrel <Relly@Terrels-MacBook-Pro.local>
2015-04-18 21:49:19 -0700

    angular album view to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,11 +33,16 @@
              templateUrl: '/templates/landing.html'
          });
 
-
            $stateProvider.state('collection', {
                url: '/collection',
                controller: 'Collection.controller',
                templateUrl: '/templates/collection.html'
+           });
+
+           $stateProvider.state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html',
+             controller: 'Album.controller'
            });
 
        }]);
@@ -70,3 +75,38 @@
                $scope.albums.push(angular.copy(albumPicasso));
            }
        }]);
+
+         blocJams.controller('Album.controller', ['$scope', function($scope) {
+           $scope.album = angular.copy(albumPicasso);
+
+           var hoveredSong = null;
+           var playingSong = null;
+
+           $scope.onHoverSong = function(song) {
+             hoveredSong = song;
+           };
+
+           $scope.offHoverSong = function(song) {
+             hoveredSong = null;
+           };
+
+           $scope.getSongState = function(song) {
+             if (song === playingSong) {
+               return 'playing';
+             }
+             else if (song === hoveredSong) {
+               return 'hovered';
+             }
+             return 'default';
+           };
+
+
+           $scope.playSong = function(song) {
+            playingSong = song;
+          };
+
+          $scope.pauseSong = function(song) {
+            playingSong = null;
+          };
+
+        }]);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -314,11 +314,16 @@ $row.find('.song-number').click(clickHandler);
              templateUrl: '/templates/landing.html'
          });
 
-
            $stateProvider.state('collection', {
                url: '/collection',
                controller: 'Collection.controller',
                templateUrl: '/templates/collection.html'
+           });
+
+           $stateProvider.state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html',
+             controller: 'Album.controller'
            });
 
        }]);
@@ -351,6 +356,41 @@ $row.find('.song-number').click(clickHandler);
                $scope.albums.push(angular.copy(albumPicasso));
            }
        }]);
+
+         blocJams.controller('Album.controller', ['$scope', function($scope) {
+           $scope.album = angular.copy(albumPicasso);
+
+           var hoveredSong = null;
+           var playingSong = null;
+
+           $scope.onHoverSong = function(song) {
+             hoveredSong = song;
+           };
+
+           $scope.offHoverSong = function(song) {
+             hoveredSong = null;
+           };
+
+           $scope.getSongState = function(song) {
+             if (song === playingSong) {
+               return 'playing';
+             }
+             else if (song === hoveredSong) {
+               return 'hovered';
+             }
+             return 'default';
+           };
+
+
+           $scope.playSong = function(song) {
+            playingSong = song;
+          };
+
+          $scope.pauseSong = function(song) {
+            playingSong = null;
+          };
+
+        }]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
