6d8b51140c2feb592844761c229fc839773b9685
Terrel <Relly@Terrels-MacBook-Pro.local>
2015-04-19 23:21:20 -0700

    added a time indicator for song progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -116,7 +116,7 @@
 
           }]);
 
-           blocJams.service('SongPlayer', function() {
+           blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
 
             var currentSoundFile = null;
 
@@ -170,25 +170,41 @@
        }
      },
 
-     setSong: function(album, song) {
+     onTimeUpdate: function(callback) {
+      return $rootScope.$on('sound:timeupdate', callback);
+    },
+
+    setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
       }
       this.currentAlbum = album;
       this.currentSong = song;
 
+
       currentSoundFile = new buzz.sound(song.audioUrl, {
         formats: [ "mp3" ],
         preload: true
       });
 
+      currentSoundFile.bind('timeupdate', function(e){
+        $rootScope.$broadcast('sound:timeupdate', this.getTime());
+      });
+
       this.play();
     }
   };
-});
+}]);
 
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
  $scope.songPlayer = SongPlayer;
+
+ SongPlayer.onTimeUpdate(function(event, time){
+   $scope.$apply(function(){
+     $scope.playTime = time;
+   });
+ });
+
 }]);
 
 //Sliders
@@ -253,7 +269,6 @@ blocJams.directive('slider', ['$document', function($document){
          }
        };
 
-
        var percentString = function () {
          var value = scope.value || 0;
          var max = scope.max || 100;
@@ -294,3 +309,33 @@ blocJams.directive('slider', ['$document', function($document){
      }
    };
  }]);
+
+
+blocJams.filter('timecode', function(){
+ return function(seconds) {
+   seconds = Number.parseFloat(seconds);
+
+     // Returned when no time is provided.
+     if (Number.isNaN(seconds)) {
+       return '-:--';
+     }
+
+     // make it a whole number
+     var wholeSeconds = Math.floor(seconds);
+
+     var minutes = Math.floor(wholeSeconds / 60);
+
+     remainingSeconds = wholeSeconds % 60;
+
+     var output = minutes + ':';
+
+     // zero pad seconds, so 9 seconds should be :09
+     if (remainingSeconds < 10) {
+       output += '0';
+     }
+
+     output += remainingSeconds;
+
+     return output;
+   };
+ });
