3bf7c7c2148024b8b0e5e7ac3d364cc3e9966c7c
Terrel <MadeByTJones@gmail.com>
2015-05-06 23:48:58 -0700

    added animations to landing albums

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,15 +1,44 @@
 $(document) .ready(function(){
 
+// Page Intro
+
   $(window).scroll(function() {
     $('#selling-points').each(function(){
       var imagePos = $(this).offset().top;
 
       var topOfWindow = $(window).scrollTop();
-      if (imagePos < topOfWindow+150) {
+      if (imagePos < topOfWindow+250) {
         $(this).addClass("slideUp");
       }
     });
   });
+
+// Landing Albums Animations
+
+    $(window).scroll(function() {
+    $('.albums-even').each(function(){
+      var imagePos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+300) {
+        $(this).addClass("slideLeft");
+      }
+    });
+  });
+
+        $(window).scroll(function() {
+    $('.albums-odd').each(function(){
+      var imagePos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+300) {
+        $(this).addClass("slideRight");
+      }
+    });
+  });
+
+    // Hover Album Flip
+
 
 
 });
